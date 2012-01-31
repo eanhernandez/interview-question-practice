e182d1f5af57a9971a24b77d9c1f77df66360ba7
eanhernandez@eanh.net <eanhernandez@eanh.net@738860cb-5d57-d6e8-ae75-5197c27a52e0>
2012-01-31 04:28:00 +0000

    moving from ec2 instance

--- trunk/rotatematrix/rotatematrix.cpp
+++ trunk/rotatematrix/rotatematrix.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
 #include<vector>
 
-void show(vector< vector<char>>* v);
+void show(std::vector< std::vector<char> >* v){};
 
 int main()
 {
 
-	vector< vector<char>>* v;
-	v
+	std::vector< std::vector<char> >* v = new  std::vector< std::vector<char> >;
+	std::vector<char>* w;
+	for (int i=0;i<10;i++)
+	{
+		w = new std::vector
+		for (int j=0;j<10;j++)
+		{
+			v->at(i).push_back('X');
+		}
+	}
 
 	std::cout << "bleh" << std::endl;
 	return 0;
