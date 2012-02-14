6a57d7bd645e2eabb4e4025cea625ad71b92a074
eanhernandez@eanh.net <eanhernandez@eanh.net@738860cb-5d57-d6e8-ae75-5197c27a52e0>
2012-02-14 03:18:27 +0000

    linked list sort

--- trunk/flipmatrix/flipmatrix.cpp
+++ trunk/flipmatrix/flipmatrix.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
 std::vector < std::vector <char> > flip(std::vector < std::vector <char> > );
-
+void dump( std::vector < std::vector <char> > v );
 int main()
 {
 
@@ -17,22 +17,28 @@ int main()
 	}
 
 	v.at(1).at(2) = 'X';
-
+	v.at(4).at(2) = 'X';
+	v.at(6).at(4) = 'X';
+	dump(v);
 	v= flip(v);
+	dump(v);
 
+	return 0;
+}
 
+void dump( std::vector < std::vector <char> > v )
+{
 	for (int i=0;i<10;i++)
 	{
 		for (int j=0;j<10;j++)
 		{
-			std::cout << v.at(i).at(j) << " "; 
+			std::cout << v.at(j).at(i) << " "; 
 		}
 		std::cout << std::endl;
 	}
-
-	std::cout<< "fwah!" <<std::endl;
-	return 0;
+	std::cout << std::endl;
 }
+
 std::vector < std::vector <char> > flip(std::vector < std::vector <char> > v)
 {
 	for (int i=0; i<v.size();i++)
@@ -42,23 +48,12 @@ std::vector < std::vector <char> > flip(std::vector < std::vector <char> > v)
 			if (v.at(i).at(j) == 'X')
 			{
 				for (int k=0;k<v.at(i).size();k++)
-					{
+				{
 						v.at(i).at(k) = '!';
-					}
+				}
 			}
 		}
 	}
+	return v;
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
